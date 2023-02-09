baa6821ed570eb61179e9570236a433133aaab47
Mr Jose Sanchez <leandrosanchez.it@gmail.com>
2023-02-09 16:00:14 -0400

    [Added] link to src code on github

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ const App = () => {
 
   return (
     <div className='flex justify-center flex-col items-center h-screen'>
-      <h1 className='text-3xl font-bold undelrine mb-10'>Barcode Generator</h1>
+      <header className='mb-10 text-center'>
+      <h1 className='text-3xl font-bold'>Barcode Generator</h1>
+      <a href='https://github.com/mrsanchez02/barcode-generator-app' target='_blank' rel='noreferrer'>Github code</a>
+      </header>
       <Input inputValue={data} inputChange={setData} />
       <BarCodeRenderer text={data}/>
     </div>
